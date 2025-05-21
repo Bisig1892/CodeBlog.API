7be5beb8ba4376b28879785320523f1aff619cf9
Tyler Bisig <bisig1892@hotmail.com>
2025-05-21 14:08:19 -0700

    implemented CreatedCategory, CategoryDto, and CreateCategoryRequestDto

---
+++ CodeBlog.API/Controllers/CategoriesController.cs
@@ -0,0 +1,46 @@
+﻿using CodeBlog.API.Data;
+using CodeBlog.API.Models.Domain;
+using CodeBlog.API.Models.DTO;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CodeBlog.API.Controllers
+{
+    // https://localhost:xxxx/api/categories
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly ApplicationDbContext dbContext;
+
+        public CategoriesController(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+            // Constructor logic (if any)
+        }
+        // GET: api/categories
+        [HttpPost]
+        public async Task<IActionResult> CreateCategory(CreateCategoryRequestDto request)
+        {
+            // Map DTO to Domain Model
+            var category = new Category
+            {
+                Name = request.Name,
+                URLHandle = request.URLHandle
+            };
+
+            await dbContext.Categories.AddAsync(category);
+            await dbContext.SaveChangesAsync();
+
+            // Domain model to DTO
+            var response = new CategoryDto
+            {
+                Id = category.Id,
+                Name = category.Name,
+                URLHandle = category.URLHandle
+            };
+
+            return Ok(response);
+        }
+    }
+}
